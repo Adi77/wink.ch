9693474ea0c4ddef77aae404e578acd964a25080
Adrian Felder <adrianfelder@gmx.ch>
2022-09-18 21:06:02 +0200

    uncommented cursor-loader markup in page.php and set cursor-loader to display none in css

--- divi-child/page.php
+++ divi-child/page.php
@@ -81,8 +81,8 @@ $is_page_builder_used = et_pb_is_pagebuilder_used(get_the_ID());
 			</svg>
 		</a>
 	</span>
-	<!--    <span id="cursor-shadow" class="cursor-shadow"></span>
-	<div id="loader"></div> -->
+	 <span id="cursor-shadow" class="cursor-shadow"></span>  
+	<!--   <div id="loader"></div>-->
 </div> <!-- #main-content -->
 
 <?php
